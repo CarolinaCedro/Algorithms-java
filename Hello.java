b10a094c54c1295e50f0d172f40d1c831884767c
Cedro <yotechgirl@gmail.com>
2022-04-15 20:49:25 -0300

    feat:two

--- Hello.java
+++ Hello.java
@@ -1,6 +1,34 @@
+import java.util.*;
+
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("hello");
+
+    Scanner scanf = new Scanner(System.in);
+
+    String name, adress, city, state, profession;
+    int cep, phone;
+
+    System.out.print("your name:");
+    name = scanf.nextLine();
+
+    System.out.print("your adress:");
+    adress = scanf.nextLine();
+
+    System.out.print("your city:");
+    city = scanf.nextLine();
+
+    System.out.print("your state:");
+    state = scanf.nextLine();
+
+    System.out.print("your profession:");
+    profession = scanf.nextLine();
+
+    System.out.print("your Cep:");
+    cep = scanf.nextInt();
+
+    System.out.print("your phone:");
+    phone = scanf.nextInt();
+
   }
 
 }
