76f86d202b0ea86f18eac61d3368ce3a97792a66
CarolinaCedro <yotechgirl@gmail.com>
2022-04-21 16:33:32 -0300

    feat: numerosInteiros

---
+++ Repetição/NumerosInteiros.java
@@ -0,0 +1,18 @@
+
+/**
+ * Suponha que você precise criar um programa para imprimir uma lista de todos os
+números  de 1 a 100. 
+
+ */
+
+import java.util.Scanner;
+
+public class NumerosInteiros {
+  public static void main(String[] args) {
+    Scanner scanf = new Scanner(System.in);
+
+    for (int i = 1; i <= 100; i++) {
+      System.out.println(i);
+    }
+  }
+}
