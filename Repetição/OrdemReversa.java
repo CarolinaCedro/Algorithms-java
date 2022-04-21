ed4d537b68f4df93cf7e5aefe3e62524c291da16
CarolinaCedro <yotechgirl@gmail.com>
2022-04-21 17:21:24 -0300

    feat: ordem reversa ok

---
+++ Repetição/OrdemReversa.java
@@ -0,0 +1,13 @@
+/**
+ * Escreva um programa que conte em ordem reversa, de 25 a zero.
+ */
+
+public class OrdemReversa {
+
+  public static void main(String[] args) {
+    for (int i = 25; i >= 0; i--) {
+      System.out.println(i);
+    }
+  }
+
+}
