9c74258fe8ced497a459d2d516c0054691131d3c
Carolina Cedro <75391803+CarolinaCedro@users.noreply.github.com>
2022-05-06 12:36:27 -0300

    teste

--- Sequencial/CalculoMinimoLitros.java
+++ Sequencial/CalculoMinimoLitros.java
@@ -5,46 +5,55 @@ public class CalculoMinimoLitros {
   void calculoLitros(float distanciaPercorrida, float consumo, int numeroVoltas, int numeroReabastecimento) {
 
     float totalPercurso, distReabastecimento, totalCombustivel, totalDistancia;
+
     totalDistancia = (distanciaPercorrida * numeroVoltas);
+
     totalPercurso = totalDistancia / 1000;
+
     distReabastecimento = (totalPercurso / numeroReabastecimento);
+
     totalCombustivel = (distReabastecimento / consumo);
 
-    System.out.println("Total de combustivel ate o primeiro reabastecimento: " + totalCombustivel);
+    System.out.println("Total de combustivel ate o primeiro reabastecimento  " + totalCombustivel);
+
   }
 
   public static void main(String[] args) {
-    try (Scanner scanf = new Scanner(System.in)) {
-      float distanciaPercorrida, consumo;
 
-      int numeroVoltas, numeroReabastecimento, cond = 1;
-      while (cond == 1) {
+    Scanner scanf = new Scanner(System.in);
 
-        System.out.println("Bem vindo ao sistema de Calculo mínimo de litros por tanque !!!");
-        System.out.println("Deseja fazer uma operação:");
-        System.out.println("(1)Sim");
-        System.out.println("(0)Não");
-        cond = scanf.nextInt();
+    float distanciaPercorrida, consumo;
+    int numeroVoltas, numeroReabastecimento, cond = 1;
 
-        if (cond == 1) {
-          System.out.println("Entre com a Distancia percorrida: ");
-          distanciaPercorrida = scanf.nextFloat();
+    while (cond == 1) {
 
-          System.out.println("Entre com a quantidade de voltas percorridas: ");
-          numeroVoltas = scanf.nextInt();
+      System.out.println("Bem vindo ao sistema de Calculo mínimo de litros por tanque !!!");
 
-          System.out.println("Quantas vezes será reabastecido: ");
-          numeroReabastecimento = scanf.nextInt();
+      System.out.println("Deseja fazer uma operação: ");
+      System.out.println("(1) Sim");
+      System.out.println("(0) Não");
+      cond = scanf.nextInt();
 
-          System.out.println("Qual o consumo por km: ");
-          consumo = scanf.nextFloat();
+      if (cond == 1) {
+        System.out.println("Entre com a distancia percorrida: ");
+        distanciaPercorrida = scanf.nextFloat();
 
-          CalculoMinimoLitros calculoLitrosFunction = new CalculoMinimoLitros();
-          calculoLitrosFunction.calculoLitros(distanciaPercorrida, consumo, numeroVoltas, numeroReabastecimento);
-        } else {
-          System.out.println("Sistema finalizado");
-        }
+        System.out.println("Entre com a quantidade de voltas percorridas:  ");
+        numeroVoltas = scanf.nextInt();
 
+        System.out.println("Quantas vezes será reabastecido: ");
+        numeroReabastecimento = scanf.nextInt();
+
+        System.out.println("Qual consumo por km: ");
+        consumo = scanf.nextFloat();
+
+        CalculoMinimoLitros calculolitrosFunction = new CalculoMinimoLitros();
+
+        calculolitrosFunction.calculoLitros(distanciaPercorrida, consumo, numeroVoltas, numeroReabastecimento);
+
+      } else {
+
+        System.out.println("Sistema Finalizado");
       }
 
     }
